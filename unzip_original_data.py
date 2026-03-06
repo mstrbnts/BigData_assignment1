67819e1d0804155306a13601df9e0fa1add33f94
puenktchien <sievers.jule@gmx.de>
2026-03-06 19:16:03 +0100

    Change location unzipped data.

--- unzip_original_data.py
+++ unzip_original_data.py
@@ -1,9 +1,10 @@
-from config import PREP, DATA
 import zipfile
 
-PREP.mkdir(parents=True, exist_ok=True)
-path_to_zip_file = DATA / "original_data" / "data.zip"
-directory_to_extract_to = PREP
+from config import DATA
 
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+DATA.mkdir(parents=True, exist_ok=True)
+path_to_zip_file = DATA / "original_data" / "data.zip"
+directory_to_extract_to = DATA / "original_data"
+
+with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
     zip_ref.extractall(directory_to_extract_to)
